ddf937fc0606b918e45071c3b1b4c6a7af456b86
Vladislav <d1xys@bk.ru>
2023-03-06 23:35:22 +0300

    ne poluchilos

--- pr2/pr2/Program.cs
+++ pr2/pr2/Program.cs
@@ -1,1 +1,77 @@
-﻿
+﻿using System;
+
+static void Main()
+{
+    solveSqrtExpression solveSqrt = new solveSqrtExpression();
+
+    void sqrtExpression(int a, int b, int c)
+    {
+        solveSqrt.setSqrt1(a, b, c);
+        solveSqrt.setSqrt2(a, b, c);
+    }
+
+    sqrtExpression(1, 7, -1);
+
+    Console.WriteLine("Корень 1" + solveSqrt.getSqrt1());
+    Console.WriteLine("Корень 2" + solveSqrt.getSqrt2());
+    Console.ReadKey();
+}
+
+class solveSqrtExpression
+{
+    private double sqrt1, sqrt2;
+
+    public double getSqrt1()
+    {
+        return sqrt1;
+    }
+
+    public double getSqrt2()
+    {
+        return sqrt2;
+    }
+
+    public void setSqrt1(int a, int b, int c)
+    { 
+        double d = (b * b) - 4 * a * c;
+        if (getSolving(a, b, c, d).Length > 0)
+        {
+            this.sqrt1 = getSolving(a, b, c, d)[0];
+        }
+
+        
+    }
+
+    public void setSqrt2(int a, int b, int c)
+    {
+        double d = (b * b) - 4 * a * c;
+
+        if (getSolving(a, b, c, d).Length > 0)
+        {
+            this.sqrt2 = getSolving(a, b, c, d)[1];
+        }
+    }
+
+    private double[] getSolving(int a, int b, int c, double d)
+    {
+        if (d > 0)
+        {
+            double sqrt1 = (-b + Math.Sqrt(d)) / (2 * a);
+            double sqrt2 = (-b - Math.Sqrt(d)) / (2 * a);
+            double[] sqrts = {sqrt1, sqrt2};
+            return sqrts;
+        }
+        else if (d == 0)
+        {
+            double sqrt1 = (-b + Math.Sqrt(d)) / (2 * a);
+            double[] sqrts = { sqrt1 };
+            return sqrts;
+        }
+        else
+        {
+            double[] sqrts = { };
+            return sqrts;
+        }
+    }
+}
+
