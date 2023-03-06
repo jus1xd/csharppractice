91bfb4b185fd5c569595ab796500d44dcd540b3d
Vladislav <d1xys@bk.ru>
2023-03-07 00:48:41 +0300

    ex1 completed

--- pr2/pr2/Program.cs
+++ pr2/pr2/Program.cs
@@ -1,77 +1,85 @@
-﻿using System;
-
-static void Main()
+﻿
+solveSqrtExpression solveSqrt = new solveSqrtExpression();
+void sqrtExpression(int a, int b, int c)
 {
-    solveSqrtExpression solveSqrt = new solveSqrtExpression();
-
-    void sqrtExpression(int a, int b, int c)
-    {
-        solveSqrt.setSqrt1(a, b, c);
-        solveSqrt.setSqrt2(a, b, c);
-    }
-
-    sqrtExpression(1, 7, -1);
-
-    Console.WriteLine("Корень 1" + solveSqrt.getSqrt1());
-    Console.WriteLine("Корень 2" + solveSqrt.getSqrt2());
-    Console.ReadKey();
+    solveSqrt.setDiscr(a, b, c);
+    solveSqrt.setSqrt1(a, b, c);
+    solveSqrt.setSqrt2(a, b, c);
 }
+
+//sqrtExpression(9, -6, 2); // нет корней
+ sqrtExpression(-1, 7, 8); // -1, 8
+//sqrtExpression(2, 5, -7); // 
+
+Console.WriteLine("Дискриминант  " + solveSqrt.getDiscr());
+if (solveSqrt.getSqrt1() == -999999999 && solveSqrt.getSqrt2() == -999999999)
+{
+    Console.WriteLine("Корней нет");
+}
+else if (solveSqrt.getSqrt1() == -999999999 || solveSqrt.getSqrt2() == -999999999)
+{
+    if (solveSqrt.getSqrt1() == -999999999)
+    {
+        Console.WriteLine("Корень 1: Корня нет");
+    } else
+    {
+        Console.WriteLine("Корень 2: Корня нет");
+    }
+} else
+{
+    Console.WriteLine("Корень 1  " + solveSqrt.getSqrt1());
+    Console.WriteLine("Корень 2  " + solveSqrt.getSqrt2());
+}
+
+Console.ReadKey();
 
 class solveSqrtExpression
 {
-    private double sqrt1, sqrt2;
-
+    private double sqrt1, sqrt2, discr;
+    
     public double getSqrt1()
     {
         return sqrt1;
     }
-
     public double getSqrt2()
     {
         return sqrt2;
     }
-
-    public void setSqrt1(int a, int b, int c)
-    { 
-        double d = (b * b) - 4 * a * c;
-        if (getSolving(a, b, c, d).Length > 0)
-        {
-            this.sqrt1 = getSolving(a, b, c, d)[0];
-        }
-
-        
+    public double getDiscr()
+    {
+        return discr;
     }
-
+    public void setSqrt1(int a, int b, int c)
+    {
+        this.sqrt1 = CalculateRoots(a, b, getDiscr())[0];
+    }
     public void setSqrt2(int a, int b, int c)
     {
-        double d = (b * b) - 4 * a * c;
-
-        if (getSolving(a, b, c, d).Length > 0)
-        {
-            this.sqrt2 = getSolving(a, b, c, d)[1];
-        }
+        this.sqrt2 = CalculateRoots(a, b, getDiscr())[1];
     }
-
-    private double[] getSolving(int a, int b, int c, double d)
+    public void setDiscr(int a, int b, int c)
+    {
+        this.discr = (b * b) - 4 * a * c;
+    }
+    private double[] CalculateRoots(int a, int b, double d)
     {
         if (d > 0)
         {
             double sqrt1 = (-b + Math.Sqrt(d)) / (2 * a);
             double sqrt2 = (-b - Math.Sqrt(d)) / (2 * a);
-            double[] sqrts = {sqrt1, sqrt2};
+            double[] sqrts = { sqrt1, sqrt2 };
             return sqrts;
         }
         else if (d == 0)
         {
             double sqrt1 = (-b + Math.Sqrt(d)) / (2 * a);
-            double[] sqrts = { sqrt1 };
+            double[] sqrts = { sqrt1, -999999999 };
             return sqrts;
         }
         else
         {
-            double[] sqrts = { };
+            double[] sqrts = {-999999999, -999999999 };
             return sqrts;
         }
     }
 }
-
