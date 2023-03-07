da0249fc0bc1307afdb37e8721a35157950baab4
Vladislav <d1xys@bk.ru>
2023-03-08 02:31:14 +0300

    2 and 3 exs completed

---
+++ pr2/pr2ex2/pr2/Program.cs
@@ -0,0 +1,44 @@
+﻿nOperations n = new nOperations();
+n.predictNumber();
+while (true)
+{
+    int input = Convert.ToInt32(Console.ReadLine());
+    if (n.setNumber(input))
+    {
+        n.predictNumber();
+    }
+    else
+    {
+        Console.WriteLine("Вы ошиблись");
+        break;
+    }
+}
+
+
+class nOperations {
+    private int n = 0;
+
+    public int getNumber () {
+        return n;
+    }
+
+    public void resetNumber () {
+        n = 0;
+    }
+
+    public bool setNumber (int n) {
+        if (this.n+1 == n) {
+            this.n = n;
+            return true;
+        } else {
+            resetNumber();
+            return false;
+        }
+    }
+
+    public void predictNumber () {
+        Console.WriteLine("Введите число " + (this.n+1));
+    }
+}
+
+   
---
+++ pr2/pr2ex3/pr2/Program.cs
@@ -0,0 +1,34 @@
+﻿class CaesarCipher
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите текст для шифрования");
+        string text = Console.ReadLine();
+        Console.WriteLine("Введите ключ");
+        int key = Convert.ToInt32(Console.ReadLine());
+        string encodedText = Encode(text, key);
+        Console.WriteLine("Зашифрованный текст: " + encodedText);
+        Console.WriteLine("Расшифрованный текст: " + Decode(encodedText, key));
+        Console.ReadLine();
+    }
+
+    static string Encode(string text, int key)
+    {
+        string encodedText = "";
+        for (int i = 0; i < text.Length; i++)
+        {
+            encodedText += (char)(text[i] + key);
+        }
+        return encodedText;
+    }
+
+    static string Decode(string text, int key)
+    {
+        string decodedText = "";
+        for (int i = 0; i < text.Length; i++)
+        {
+            decodedText += (char)(text[i] - key);
+        }
+        return decodedText;
+    }
+}
